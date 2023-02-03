3be3ef557f76541ab0df6c771c7f83df1af3ba0c
Ankush Dutta <ankush.ad050203@gmail.com>
2023-02-04 02:06:14 +0530

    Navbar completed

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+import group1 from '../images/group1.svg';
+import group2 from '../images/group3.svg';
+const Navbar = () => {
+  return (
+    <div className='flex flex-row w-[1415px] h-[83px] mt-[0px]'> 
+        <label className='text-blue1 w-[125px] h-[29px] mt-[27px] ml-[24px] leading-7 font-bold text-2xl '>Dashboard</label>
+        <Image src={group1} alt="group" className='w-[97px] h-[17px] ml-[1090px] mt-[32px]'/>
+        <Image src={group2} alt="group2" className='w-[19px] h-[19.71px] ml-[10px] mt-[29px] mr-[44px]'/>
+    </div>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors : {
+      'green1' : '#7DCE13',
+      'green2' : '#3EC70B',
+      'green3' : '#2B7A0B',
+      'yellow1' : '#EAE509',
+      'white1' : '#FFFFFF',
+      'grey1' : '#e5e7eb',
+      'black1' : '#000000',
+      'white2':'#F5F5F5',
+      'blue1' : '#252F40',
+    },
     extend: {},
   },
   plugins: [],
