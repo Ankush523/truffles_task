4a6a66fa67cc40765a5af9c0946d38e84a33a145
Ankush Dutta <ankush.ad050203@gmail.com>
2023-02-05 11:24:16 +0530

    redirection to homepage

--- components/LoginPageComponents/LoginNavbar.js
+++ components/LoginPageComponents/LoginNavbar.js
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
 import truffleIcon from '@/images/truffleIcon2.png';
+import Link from 'next/link';
 const LoginNavbar = () => {
   return (
     <div className='w-100vw h-[72px] mt-[0px] ml-[0px] bg-white1 flex flex-row'>
-        <Image src={truffleIcon} alt="logo" className='w-[166px] h-[28.25px] ml-[49px] mt-[22px] mb-[22px]'/>
+        <Link href={"/HomePage"}>
+          <Image src={truffleIcon} alt="logo" className='w-[166px] h-[28.25px] ml-[49px] mt-[22px] mb-[22px]'/>
+        </Link>
         <div className='flex flex-row'>
             <label className='ml-[1211px] text-blue1 text-sm mt-[28px]'>Having trouble?</label>
             <label className='text-purple2 text-sm mt-[28px] ml-[10px]'>Get Help</label>
